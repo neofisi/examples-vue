c3b0f44f3aeb7e07d6df341444603bc48fa5780a
Abraham C <neo_fisi@hotmail.com>
2020-07-05 23:48:16 -0500

    first commit example vue

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const app  = new Vue({
+    el:'#app',
+    data:{
+        titulo:'hola mundo con Vue',
+        frutas:[
+               {nombre:'Manzanas', estado:"false"},
+               {nombre:'Platanos', estado:"true"},
+               {nombre:'Fresas', estado:"false"}
+            ]
+    }   
+   })
